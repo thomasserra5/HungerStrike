537640b65f2bea6126cb0971b3e6f6cfe3187811
thomasserra5 <thomasserra5@protonmail.com>
2026-02-24 18:08:19 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function collapseButton() {
+        let test = document.getElementById('collapsed')
+  if (test.style.display == "block"){
+    test.style.display = "none";
+  }
+  else{
+    test.style.display = "block";
+  }
+}
